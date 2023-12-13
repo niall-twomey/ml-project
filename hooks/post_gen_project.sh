666d6c7ba71a0345799170e655a4794467cc8298
niall-twomey <59443813+niall-twomey@users.noreply.github.com>
2023-12-13 21:02:59 +0000

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-# Set up git environment 
+# Activate environment
+PROJECT_NAME = "{{ cookiecutter.project_name }}"
+ENVIRONMENT_TYPE = "{{ cookiecutter.environment }}"
+PYTHON_VERSION = "{{ cookiecutter.python_version }}"
+
+# Set up git  
 git init 
 
-# Set up the pre-commit hook environment 
-pip install pre-commit 
+# Create environment if needed
+echo "Setting up environment..."
+if [ "${ENVIRONMENT_TYPE}" = "conda" ]; then
+  if conda info --envs | grep -q "${PROJECT_NAME}"; then 
+    echo "Conda environment already exists. Skipping."
+  else
+    echo "Creating new conda environment" 
+    conda create --name "${}" python="${PYTHON_VERSION}"
+  fi 
+  
+  conda activate "${PROJECT_NAME}"
+  
+  conda install pre-commit 
+else
+  pip install pre-commit 
+fi
+
+# Install pre-commit hooks 
 pre-commit install 
 
-# Install the pipenv environment
-# pipenv install --dev 
-
+echo export`which python` >> .env
