cd4335f12125c6142c6061f53abc211ca37f445c
niall-twomey <59443813+niall-twomey@users.noreply.github.com>
2023-12-13 21:24:15 +0000

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -12,25 +12,34 @@ git init
 # Create environment if needed
 echo "Setting up environment..."
 if [ "$ENVIRONMENT_TYPE" = "conda" ]; then
+  # Ensure that the conda environment is currently active
+  conda activate 
+
   if conda info --envs | grep -q "$PROJECT_NAME"; then 
     echo "Conda environment already exists. Skipping."
   else
     echo "Creating new conda environment" 
     conda create --name "$PROJECT_NAME" python="$PYTHON_VERSION" -y
   fi 
-  
+
+  echo "Activating new env..."
   conda activate "$PROJECT_NAME"
-  
+
+  echo "Installing pre-commit via conda..." 
   conda install pre-commit -y
 else
+  echo "Installing pre-commit via pip..." 
   pip install pre-commit 
 fi
 
 # Install pre-commit hooks 
+echo "Installing pre-commit hooks to git module..." 
 pre-commit install 
 
 # Install the optional libraries
 if [ "$INSTALL_RECOMMENDED" = "yes" ]; then 
+  echo "Installing dependencies..."
+  
   if conda info --envs | grep -q "$PROJECT_NAME"; then 
     conda install numpy scipy pandas matplotlib seaborn scikit-learn pyyaml tqdm jupyter ipython -y
   else
