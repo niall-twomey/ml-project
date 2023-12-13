478573d7ec19701168e82ca7ec9caa1b65008292
niall-twomey <59443813+niall-twomey@users.noreply.github.com>
2023-12-13 21:21:37 +0000

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Activate environment
+# Define variables
 PROJECT_NAME="{{ cookiecutter.project_name }}"
 ENVIRONMENT_TYPE="{{ cookiecutter.environment }}"
 PYTHON_VERSION="{{ cookiecutter.python_version }}"
+INSTALL_RECOMMENDED="{{ cookiecutter.install_recommended }}"
 
-# Set up git  
+# Initialise git  
 git init 
 
 # Create environment if needed
@@ -15,12 +16,12 @@ if [ "$ENVIRONMENT_TYPE" = "conda" ]; then
     echo "Conda environment already exists. Skipping."
   else
     echo "Creating new conda environment" 
-    conda create --name "$PROJECT_NAME" python="$PYTHON_VERSION"
+    conda create --name "$PROJECT_NAME" python="$PYTHON_VERSION" -y
   fi 
   
   conda activate "$PROJECT_NAME"
   
-  conda install pre-commit 
+  conda install pre-commit -y
 else
   pip install pre-commit 
 fi
@@ -28,4 +29,13 @@ fi
 # Install pre-commit hooks 
 pre-commit install 
 
-echo export`which python` >> .env
+# Install the optional libraries
+if [ "$INSTALL_RECOMMENDED" = "yes" ]; then 
+  if conda info --envs | grep -q "$PROJECT_NAME"; then 
+    conda install numpy scipy pandas matplotlib seaborn scikit-learn pyyaml tqdm jupyter ipython -y
+  else
+    pip install numpy scipy pandas matplotlib seaborn scikit-learn pyyaml tqdm jupyter ipython
+  fi 
+fi 
+
+
